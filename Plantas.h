c3becd8b36cb5f53a7eb8744ce552cec9d280163
luanarabello <luanasrabello@gmail.com>
2025-11-01 00:16:27 +0000

    inicio da classe e subclasses de plantas

--- Plantas.h
+++ Plantas.h
@@ -4,5 +4,56 @@
 
 #ifndef PLANTAS_H
 #define PLANTAS_H
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Plantas
+{
+protected:
+	int nutrientes;
+	int agua;
+	string beleza;
+public:
+	Plantas(int nutrientes, int agua, string beleza);
+	virtual void absorveAgua(int &agua);
+	virtual void absorveNutrientes(int &nutrientes);
+	virtual void multiplica(int &nutrientes, int &agua);
+	virtual void morre(); // precisa mandar a quantidade de agua e nutri no solo
+	~Plantas();
+};
+
+class Cacto : public Plantas
+{
+public:
+	void absorveAgua(int &agua) override;
+	void absorveNutrientes(int &nutrientes) override;
+	void multiplica(int &nutrientes, int &agua) override;
+	void morre() override; // precisa mandar a quantidade de agua e nutri no solo
+};
+
+class Roseira : public Plantas
+{
+	void absorveAgua(int &agua) override;
+	void absorveNutrientes(int &nutrientes) override;
+	void multiplica(int &nutrientes, int &agua) override;
+	void morre() override; // precisa mandar a quantidade de agua e nutri no solo
+};
+
+class ErvaDaninha : public Plantas
+{
+	void absorveAgua(int &agua) override;
+	void absorveNutrientes(int &nutrientes) override;
+	void multiplica(int &nutrientes, int &agua) override;
+	void morre() override; // precisa mandar a quantidade de agua e nutri no solo
+};
+
+class BastaoImperador : public Plantas
+{
+	void absorveAgua(int &agua) override;
+	void absorveNutrientes(int &nutrientes) override;
+	void multiplica(int &nutrientes, int &agua) override;
+	void morre() override; // precisa mandar a quantidade de agua e nutri no solo
+};
 #endif //PLANTAS_H
